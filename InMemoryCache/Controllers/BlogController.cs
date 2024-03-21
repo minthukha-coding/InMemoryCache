6f7828d266de05256d21cd8de630cab2278e685b
minthukha-coding <minthukha1239788@gmail.com>
2024-03-21 14:20:37 +0630

    [Complete] Cache in UpdateBlog

--- InMemoryCache/Controllers/BlogController.cs
+++ InMemoryCache/Controllers/BlogController.cs
@@ -49,5 +49,50 @@ namespace InMemoryCache.Controllers
         }
 
         #endregion
+
+        #region UpdateBlog
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateBlog(int id, BlogDataModel model)
+        {
+            try
+            {
+                var item = await _appDbContext.Blogs.FirstOrDefaultAsync(x => x.Blog_Id == id);
+                if (item == null)
+                {
+                    return NotFound("No data found");
+                }
+
+                item.Blog_Title = model.Blog_Title;
+                item.Blog_Author = model.Blog_Author;
+                item.Blog_Content = model.Blog_Content;
+
+                _appDbContext.Attach(item);
+
+                int result = await _appDbContext.SaveChangesAsync();
+
+                if (result > 0)
+                {
+                    List<BlogDataModel> updatedBlogs = await _appDbContext.Blogs
+                        .OrderByDescending(x => x.Blog_Id)
+                        .ToListAsync();
+
+                    _cache.Set("blogs", updatedBlogs);
+
+                    return Ok(updatedBlogs); // Return a success response
+                }
+                else
+                {
+                    return BadRequest("Failed to update the blog entry."); // Return a failure response
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+        #endregion
     }
 }
